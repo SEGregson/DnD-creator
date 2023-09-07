01e42aacf7a73b3bf722a5db42c82ca5c82cff4e
SEGregson <skyeegregson@gmail.com>
2023-09-07 13:45:37 +0100

    re-format select screen

--- dnd-creator/src/View/char_select_screen.rs
+++ dnd-creator/src/View/char_select_screen.rs
@@ -1,23 +1,36 @@
 use std::sync::Arc;
 
-use cursive::{Cursive, views::{SelectView, Dialog, LinearLayout, TextView, TextArea, Button, DummyView}, view::Resizable};
+use cursive::{Cursive, views::{SelectView, Dialog, LinearLayout, Button, DummyView}, view::Nameable};
 
 use crate::Model::character_manager::CharacterManager;
 
 pub fn select_char_view(ui: &mut Cursive, char_manager: Arc<CharacterManager>) {
+    fn view_sheet(ui: &mut Cursive) {}
+    fn edit_sheet(ui: &mut Cursive) {}
+    fn delete_sheet(ui: &mut Cursive) {}
+    fn new_sheet(ui: &mut Cursive) {}
     
-    let select_char_view = update_list(SelectView::<String>::new(), char_manager);    
-    let sample_view = Dialog::text("this is a placeholder for the viewing of char sheets")
-        .title("placeholder");
+    let select_char_view = update_list(SelectView::<String>::new(), char_manager)
+        .with_name("select_char_view");
+
+    let control_view = LinearLayout::vertical()
+        .child(Button::new("view", view_sheet))
+        .child(Button::new("edit", edit_sheet))
+        .child(Button::new("delete", delete_sheet))
+        .child(DummyView)
+        .child(Button::new("new", new_sheet));
 
     ui.add_layer(
-        LinearLayout::horizontal()
-            .child(Dialog::around(select_char_view)
-                .title("characters"))
-            .child(DummyView)
-            .child(sample_view)
+        Dialog::around(
+            LinearLayout::horizontal()
+                .child(control_view)
+                .child(select_char_view)
+            )
+            .title("characters")
+        );
+                
         
-    );
+    
 }
 
 fn update_list(mut select_char: SelectView, char_manager: Arc<CharacterManager>) -> SelectView {
